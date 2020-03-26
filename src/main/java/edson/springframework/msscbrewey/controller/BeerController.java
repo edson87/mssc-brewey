4f1066be3b4a76cd7db08544ce1dc4ae263348e9
edson <edson_lopez@hotmail.com.ar>
2020-03-26 14:43:35 -0400

    Build Put Endpoint

--- src/main/java/edson/springframework/msscbrewey/controller/BeerController.java
+++ src/main/java/edson/springframework/msscbrewey/controller/BeerController.java
@@ -34,5 +34,11 @@ public class BeerController {
         return new ResponseEntity<>(headers , HttpStatus.CREATED);
     }
 
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity<BeerDto>(beerDto, HttpStatus.NO_CONTENT);
+    }
 
 }
